5062356a98f44b57bc872ac3ca73702adf28853c
megan alonzo <megan.alonzo@loop.colum.edu>
2018-12-02 20:03:56 -0600

    door script

--- 2d doors/Assets/Scripts/Door.cs
+++ 2d doors/Assets/Scripts/Door.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Door : MonoBehaviour {
 
+    private bool isPlayerInTrigger;
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.CompareTag("player"))
@@ -13,6 +15,30 @@ public class Door : MonoBehaviour {
                 Debug.Log("player activated door");
             }
             
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player"))
+        {
+            isPlayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("activate") && isPlayerInTrigger)
+        {
+            Debug.Log("player activated door");
         }
     }
 }
--- 2d doors/Assets/Scripts/PlayerCharacter.cs
+++ 2d doors/Assets/Scripts/PlayerCharacter.cs
@@ -4,13 +4,85 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private float accelerationForce = 5;
+
+    [SerializeField]
+    private float maxSpeed = 5;
+
+    [SerializeField]
+    private float jumpForce = 10;
+
+    [SerializeField]
+    private Rigidbody2D rb2d;
+
+    [SerializeField]
+    private Collider2D playerGroundCollider;
+
+    [SerializeField]
+    private PhysicsMaterial2D playerMovingPhysicsMaterial, playerStoppingPhysicsMaterial;
+
+    [SerializeField]
+    private Collider2D groundDetectTrigger;
+
+    [SerializeField]
+    private ContactFilter2D groundContactFilter;
+
+    private float horizontalInput;
+    private bool isOnGround;
+    private Collider2D[] groundHitDetectionResults = new Collider2D[16];
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateIsOnGround();
+        UpdateHorizontalInput();
+        HandleJumpInput();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdatePhysicsMaterial();
+        Move();
+    }
+
+    private void UpdatePhysicsMaterial()
+    {
+        if (Mathf.Abs(horizontalInput) > 0)
+        {
+            playerGroundCollider.sharedMaterial = playerMovingPhysicsMaterial;
+        }
+        else
+        {
+            playerGroundCollider.sharedMaterial = playerStoppingPhysicsMaterial;
+        }
+    }
+
+    private void UpdateIsOnGround()
+    {
+        isOnGround = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetectionResults) > 0;
+        Debug.Log("IsOnGround: " + isOnGround);
+    }
+
+    private void UpdateHorizontalInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void HandleJumpInput()
+    {
+        if (Input.GetButton("jump"))
+        {
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    private void Move()
+    {
+        rb2d.AddForce(Vector2.right * horizontalInput * accelerationForce);
+        Vector2 clampedVelocity = rb2d.velocity;
+        clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+        rb2d.velocity = clampedVelocity;
+    }
 }
